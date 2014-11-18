4473a59f0e7356dfa6f1b29f12a1637167799a1f
replay <mauro.stettler@gmail.com>
2014-11-17 22:53:26 -0300

    fix tests

--- clients/blockcypher/crawler.py
+++ clients/blockcypher/crawler.py
@@ -8,6 +8,7 @@ import requests
 class Crawler(crawler_base.CrawlerBase):
     chain_head_url = 'http://api.blockcypher.com/v1/btc/main'
     block_url = 'https://api.blockcypher.com/v1/btc/main/blocks/{block}'
+    transaction_url = 'https://api.blockcypher.com/v1/btc/main/txs/{tx_id}'
     parser = parser.BlockCypherParser
     time_format = '%Y-%m-%dT%H:%M:%SZ'
 
@@ -16,8 +17,7 @@ class Crawler(crawler_base.CrawlerBase):
             datetime.datetime.strptime(time, self.time_format).timetuple())
 
     def get_transaction(self, tx_id):
-        transaction_url = 'https://api.blockcypher.com/v1/btc/main/txs/{tx_id}'
-        tx_data = requests.get(transaction_url.format(tx_id=tx_id)).json()
+        tx_data = requests.get(self.transaction_url.format(tx_id=tx_id)).json()
         return self.parser.build_transaction(tx_data)
 
     def prepare_transaction(self, txid):
--- clients/blockcypher/test/test_crawler.py
+++ clients/blockcypher/test/test_crawler.py
@@ -19,17 +19,14 @@ class CrawlerTest(unittest.TestCase):
            blockcypher format'''
         return {
             'hash': values['hash'],
-            'tx': [{
-                'out': [
-                    {'value': output['value'],
-                     'addr': output['addr']}
-                    for output in transaction['outputs']],
-                'inputs': [
-                    {'prev_out': {
-                        'value': input['value'],
-                        'addr': input['addr']}}
-                    for input in transaction['inputs']]}
-                   for transaction in values['transactions']]}
+            'outputs': [
+                {'value': output['value'],
+                 'addresses': output['addr']}
+                for output in values['outputs']],
+            'inputs': [
+                {'output_value': input['value'],
+                 'addresses': input['addr']}
+                for input in values['inputs']]}
 
     def _stub_block_dict(self, values):
         '''Generate a stub block based on given values, blockcypher format'''
@@ -45,35 +42,53 @@ class CrawlerTest(unittest.TestCase):
     def test_get_transactions_by_age(self, requests_get):
         self.setUp()
         stub_head = mock.MagicMock()
-        stub_head.json.return_value = {'hash': 'testhash1'}
+        stub_head.json.return_value = {'hash': 'block2'}
         stub_transaction1 = mock.MagicMock()
         stub_transaction1.json.return_value = self._stub_transaction_dict({
             'hash': 'transaction1',
-            'transactions': [{
-                'outputs': [
-                    {'value': 10, 'addr': 'addr1'},
-                    {'value': 20, 'addr': 'addr2'}],
-                'inputs': [
-                    {'value': 30, 'addr': 'addr3'},
-                    {'value': 40, 'addr': 'addr4'}]}]})
+            'outputs': [
+                {'value': 10, 'addr': 'addr1'},
+                {'value': 20, 'addr': 'addr2'}],
+            'inputs': [
+                {'value': 30, 'addr': 'addr3'},
+                {'value': 40, 'addr': 'addr4'}]})
 
-        stub_block1 = self._stub_block_dict({
-            'prev_block': 'block0',
-            'hash': 'block1',
+        stub_transaction2 = mock.MagicMock()
+        stub_transaction2.json.return_value = self._stub_transaction_dict({
+            'hash': 'transaction1',
+            'outputs': [],
+            'inputs': []})
+
+        stub_block1 = mock.MagicMock()
+        stub_block1.json.return_value = self._stub_block_dict({
+            'prev_block': 'block1',
+            'hash': 'block2',
             'time': calendar.timegm(time.gmtime()) - 400,
             'transaction_ids': [
                 'transaction1',
                 'transaction2']})
 
-        stub_block2 = self._stub_block_dict({
-            'prev_block': 'block1',
-            'hash': 'block2',
+        stub_block2 = mock.MagicMock()
+        stub_block2.json.return_value = self._stub_block_dict({
+            'prev_block': 'block0',
+            'hash': 'block1',
             'time': calendar.timegm(time.gmtime()) - 2000,
-            'transaction_ids': [
-                'transaction1',
-                'transaction2']})
+            'transaction_ids': []})
 
         requests_get.side_effect = [
             stub_head,
             stub_block1,
+            stub_transaction1,
+            stub_transaction2,
             stub_block2]
+
+        [x for x in self.crawler.get_transactions_by_age(1000)]
+        requests_get.assert_has_calls([
+            mock.call(self.crawler.chain_head_url),
+            mock.call(self.crawler.block_url.format(block='block2')),
+            mock.call(self.crawler.transaction_url.format(
+                tx_id='transaction1')),
+            mock.call(self.crawler.transaction_url.format(
+                tx_id='transaction2')),
+            mock.call(self.crawler.block_url.format(block='block1')),
+        ])
